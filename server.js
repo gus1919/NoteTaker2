9cca6122696eb4f5198b443f5ac95be1f69b8c48
Justin Oeters <justin.oeters@gmail.com>
2021-10-03 16:17:50 -0400

    initial server.js and npm init

---
+++ server.js
@@ -0,0 +1,25 @@
+// Dependencies
+const express = require('express');
+const apiPath = require('./routes/apiPath');
+const htmlPath = require('./routes/apihtml');
+
+// Helper method for generating unique ids
+const uuid = require('./helpers/uuid');
+
+// Set the PORT
+const PORT = process.env.PORT || 3001;
+
+// call the app
+const app = express();
+
+// encode the data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+app.use('/api', apiPath);
+app.use('/', htmlPath);
+
+// Listener
+app.listen(PORT, () =>
+  console.log(`Server is listening at http://localhost:${PORT} 🚀`));
